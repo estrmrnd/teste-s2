043ec4d45b59a0cb620df9e0bb71636a574c6b5e
Ester Miranda <esterssmiranda@gmail.com>
2024-05-12 18:02:45 -0300

    finish

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,5 +1,5 @@
 async function listaProdutos() {
-    const conexao = await fetch("https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fq=C:65&_from=1&_to=50" , {mode: 'no-cors'});
+    const conexao = await fetch("https://cantao.vtexcommercestable.com.br/api/catalog_system/pub/products/search?fq=C:65&_from=1&_to=50");
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
